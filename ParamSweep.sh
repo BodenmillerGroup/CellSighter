81b7ff6eb9b52c7c5153272ee7703722aaf12318
bpalau <bruno.palaufernandez@uzh.ch>
2023-03-22 09:17:34 +0000

    Parameter Sweep

---
+++ ParamSweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..24}
+do
+    bp="/mnt/immucan_volume/processed_data/Panel_1/CellSighter/ParamSweep/Model_$i"
+    python CellSighterCode/train.py --base_path=$bp
+    python CellSighterCode/eval.py --base_path=$bp
+    python CellSighterCode/analyze_results/confusion_matrix.py --base_path=$bpv
+    find $bp -name "*pth" -type f -delete
+done
+
--- ParamSweep2.sh
+++ ParamSweep2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for i in {1..24}
+
+for i in {3..24}
 do
     bp="/mnt/immucan_volume/processed_data/Panel_1/CellSighter/ParamSweep2/Model_$i"
-    python CellSighterCode/train.py --base_path=$bp
-    python CellSighterCode/eval.py --base_path=$bp
-    python CellSighterCode/analyze_results/confusion_matrix.py --base_path=$bpv
-    find $bp -name "*pth" -type f -delete
+    if [ -d "$bp" ]; then
+        python CellSighterCode/train.py --base_path=$bp
+        python CellSighterCode/eval.py --base_path=$bp
+        python CellSighterCode/analyze_results/confusion_matrix.py --base_path=$bpv
+        find $bp -name "*pth" -type f -delete
+    fi
 done
