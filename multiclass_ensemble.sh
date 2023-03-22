2c5804de615723bd05c1307fae7bdbd4c09680c8
bpalau <bruno.palaufernandez@uzh.ch>
2023-03-22 09:21:31 +0000

    multiclass setup and analysis

--- multiclass_ensemble.sh
+++ multiclass_ensemble.sh
@@ -5,6 +5,5 @@ do
     bp="/mnt/immucan_volume/processed_data/Panel_1/CellSighter/Multiclass/Model_$i"
     python CellSighterCode/train.py --base_path=$bp
     python CellSighterCode/eval.py --base_path=$bp
-    python CellSighterCode/analyze_results/confusion_matrix.py --base_path=$bp
     find $bp -name "*pth" -type f -delete
 done
